119768c9161b42e1dde0f5eee3ac60cdf3229f2c
Michael Monashev <postmaster@softsearch.ru>
2016-06-14 18:56:33 +0300

    Create mosk_server21.go

---
+++ mosk_server21.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"client"
+)
+
+func main() {
+
+	err := client.Mock_start_node(2, map[uint64]string{
+		1: "127.0.0.1:3001",
+		2: "127.0.0.1:3002",
+		3: "127.0.0.1:3003",
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("node 2 successful started")
+	time.Sleep(10 * time.Hour)
+}
